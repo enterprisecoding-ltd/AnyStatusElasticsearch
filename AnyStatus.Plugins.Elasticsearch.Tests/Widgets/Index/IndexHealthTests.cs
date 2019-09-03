e41c108efd4ea26949927d86081456e13f4d0631
Fatih Boy <fatih@enterprisecoding.com>
2019-09-03 22:25:09 +0300

    License changed from GPL 3.0 to Apache 2.0

--- AnyStatus.Plugins.Elasticsearch.Tests/Widgets/Index/IndexHealthTests.cs
+++ AnyStatus.Plugins.Elasticsearch.Tests/Widgets/Index/IndexHealthTests.cs
@@ -1,7 +1,22 @@
-﻿using AnyStatus.API;
+﻿/*
+Anystatus Elasticsearch plugin
+Copyright 2019 Fatih Boy
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+using AnyStatus.API;
 using AnyStatus.Plugins.Elasticsearch.ElasticsearchClient;
 using AnyStatus.Plugins.Elasticsearch.ElasticsearchClient.Objects.Cluster;
-using AnyStatus.Plugins.Elasticsearch.ElasticsearchClient.Objects.Health;
 using AnyStatus.Plugins.Elasticsearch.ElasticsearchClient.Objects.Shared;
 using AnyStatus.Plugins.Elasticsearch.Helpers;
 using AnyStatus.Plugins.Elasticsearch.Index.IndexHealth;
